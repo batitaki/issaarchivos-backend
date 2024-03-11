6d74f6dcdfb0cb6bbd275c1f810e361ee0fdd153
batitaki <taquinibautista@gmail.com>
2024-03-11 02:59:44 -0300

    associate

--- src/database/models/Category.js
+++ src/database/models/Category.js
@@ -1,34 +1,42 @@
 // models/Category.js
 
 function categoryData(sequelize, DataTypes) {
-  let categoryTableName = 'Category'; 
+  let categoryTableName = 'Category';
 
   let categoryColumns = {
-    ID: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    Name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    Description: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    }
+      ID: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      Name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      Description: {
+          type: DataTypes.TEXT,
+          allowNull: true
+      }
   };
 
   let categoryConfig = {
-    timestamps: false,
-    tableName: 'Category', 
+      timestamps: false,
+      tableName: 'Category',
   };
 
   const Category = sequelize.define(
-    categoryTableName,
-    categoryColumns,
-    categoryConfig
+      categoryTableName,
+      categoryColumns,
+      categoryConfig
   );
+
+  // Definir asociación hasMany con Product
+  Category.associate = function(models) {
+      Category.hasMany(models.Product, {
+          as: 'Products',
+          foreignKey: 'CategoryID' // Suponiendo que 'CategoryID' es la clave foránea en la tabla de Product que hace referencia a la tabla de Category
+      });
+  };
 
   return Category;
 }
