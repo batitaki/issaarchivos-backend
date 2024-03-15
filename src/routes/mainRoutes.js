2e7a9a160743526866d9de685eab634513aced84
batitaki <taquinibautista@gmail.com>
2024-03-15 13:41:41 -0300

    root res.send

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 const productRoutes = require("./productRoutes")
 
+router.get("/", (req, res) => {
+    res.send("Welcome to the root URL");
+  });
+
 router.use("/products", productRoutes);
 // Agrega otras rutas según sea necesario
 
