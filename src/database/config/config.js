32d972b5d2ca14654fdb3612705eeb5c2eda9cc2
batitaki <taquinibautista@gmail.com>
2024-04-17 15:46:19 -0300

    commit for production

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,18 +7,19 @@ module.exports = {
       "dialect": "mysql"
     },
     "test": {
-      "username": "root",
-      "password": null,
-      "database": "database_test",
-      "host": "mysql-holograma.alwaysdata.net",
+      "username": "351199",
+      "password": "issarchivos",
+      "database": "issaarchivos_2000",
+      "host": "mysql-issaarchivos.alwaysdata.net",
       "dialect": "mysql"
     },
     "production": {
-      "username": "root",
-      "password": null,
-      "database": "database_production",
-      "host": "mysql-holograma.alwaysdata.net",
+      "username": "351199",
+      "password": "issarchivos",
+      "database": "issaarchivos_2000",
+      "host": "mysql-issaarchivos.alwaysdata.net",
       "dialect": "mysql"
     }
   }
+  
   
