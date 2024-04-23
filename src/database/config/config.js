2a4497c39b864afd44927b0891e818cd20bc9208
batitaki <taquinibautista@gmail.com>
2024-04-23 13:58:12 -0300

    entornos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,25 +1,26 @@
+require('dotenv').config();
+
 module.exports = {
-    "development": {
-      "username": "351199",
-      "password": "issarchivos",
-      "database": "issaarchivos_2000",
-      "host": "mysql-issaarchivos.alwaysdata.net",
-      "dialect": "mysql"
-    },
-    "test": {
-      "username": "351199",
-      "password": "issarchivos",
-      "database": "issaarchivos_2000",
-      "host": "mysql-issaarchivos.alwaysdata.net",
-      "dialect": "mysql"
-    },
-    "production": {
-      "username": "351199",
-      "password": "issarchivos",
-      "database": "issaarchivos_2000",
-      "host": "mysql-issaarchivos.alwaysdata.net",
-      "dialect": "mysql"
-    }
+  "development": {
+    "username": process.env.DEV_DB_USERNAME,
+    "password": process.env.DEV_DB_PASSWORD,
+    "database": process.env.DEV_DB_DATABASE,
+    "host": "mysql-issaarchivos.alwaysdata.net",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.TEST_DB_USERNAME,
+    "password": process.env.TEST_DB_PASSWORD,
+    "database": process.env.TEST_DB_DATABASE,
+    "host": "mysql-issaarchivos.alwaysdata.net",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.PROD_DB_USERNAME,
+    "password": process.env.PROD_DB_PASSWORD,
+    "database": process.env.PROD_DB_DATABASE,
+    "host": "mysql-issaarchivos.alwaysdata.net",
+    "dialect": "mysql"
   }
-  
-  
+};
+
